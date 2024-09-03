b1990a240adcd847427cf87167599511dcb6f363
Siaka385 <teddysiaka20@gmail.com>
2024-09-03 06:30:27 +0300

    add:days of week

--- dr-strange.js
+++ dr-strange.js
@@ -4,12 +4,12 @@ var addWeek=(date)=>{
     var fullyear=(dates)=>{
            var startyear=epoch.split("-")[0];
            var endyear=dates.split("-")[0];
-        return parseInt(endyear)-parseInt(startyear)
+        return (parseInt(endyear)-parseInt(startyear))*365
     }
      var months=(dates)=>{
         var starrmonth=epoch.split("-")[1];
         var endmonth=dates.split("-")[1];
-     return parseInt(endmonth)-parseInt(starrmonth)
+     return (parseInt(endmonth)-parseInt(starrmonth))*30
      }
      var days=(dates)=>{
         var startdate=epoch.split("-")[2];
@@ -17,7 +17,19 @@ var addWeek=(date)=>{
      return parseInt(enddate)-parseInt(startdate)
      }
 
-
-    return months(date)
+     var daysOfweek = [];
+     var firstWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+     var secondWeek = firstWeek.map(day => "second" + day);
+     daysOfweek = firstWeek.concat(secondWeek);
+     var totaldays= fullyear(date)+months(date)+days(date)
+     console.log(totaldays)
+     if (totaldays<14){
+        return daysOfweek[totaldays+1]
+     }else if(totaldays%14 === 0){
+        return daysOfweek[0]
+     }else{
+          let day=totaldays%14 
+              return daysOfweek[day]
+     }
 }
-console.log(addWeek("0001-12-01"))
+console.log(addWeek("0001-01-16"))
